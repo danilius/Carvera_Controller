a0f2bf0c22d919e1ba40b93b4ecea4a628e7b066
Warioo <mark.zehender@googlemail.com>
2025-07-03 23:57:55 +0200

    fixed the set origin popup to calculate the offsets on pressing the different options

--- carveracontroller/main.py
+++ carveracontroller/main.py
@@ -315,9 +315,12 @@ class OriginPopup(ModalView):
             if self.coord_popup.config['origin']['anchor'] == 2:
                 x = round(CNC.vars['wcox'] + laser_x - CNC.vars["anchor1_x"] - CNC.vars["anchor2_offset_x"], 4)
                 y = round(CNC.vars['wcoy'] + laser_y - CNC.vars["anchor1_y"] - CNC.vars["anchor2_offset_y"], 4)
-            else:
+            elif self.coord_popup.config['origin']['anchor'] == 1:
                 x = round(CNC.vars['wcox'] + laser_x - CNC.vars["anchor1_x"], 4)
                 y = round(CNC.vars['wcoy'] + laser_y - CNC.vars["anchor1_y"], 4)
+            else:
+                x = 0
+                y = 0
         self.txt_x_offset.text = str(x)
         Utils.bind_auto_select_to_text_input(self.txt_x_offset)
         self.txt_y_offset.text = str(y)
@@ -331,6 +334,27 @@ class OriginPopup(ModalView):
         elif self.cbx_current_position.active:
             return 4
         return 1
+    
+    def update_offsets(self):
+        # Use the same logic as CoordPopup.load_origin_label to set offsets
+        app = App.get_running_app()
+        if app.has_4axis:
+            x = round(CNC.vars["wcox"] - CNC.vars['anchor1_x'] - CNC.vars['rotation_offset_x'], 4)
+            y = round(CNC.vars['wcoy'] - CNC.vars['anchor1_y'] - CNC.vars['rotation_offset_y'], 4)
+        else:
+            laser_x = CNC.vars['laser_module_offset_x'] if CNC.vars['lasermode'] else 0.0
+            laser_y = CNC.vars['laser_module_offset_y'] if CNC.vars['lasermode'] else 0.0
+            if self.cbx_anchor1.active:
+                x = round(CNC.vars['wcox'] + laser_x - CNC.vars["anchor1_x"], 4)
+                y = round(CNC.vars['wcoy'] + laser_y - CNC.vars["anchor1_y"], 4)
+            elif self.cbx_anchor2.active:
+                x = round(CNC.vars['wcox'] + laser_x - CNC.vars["anchor1_x"] - CNC.vars["anchor2_offset_x"], 4)
+                y = round(CNC.vars['wcoy'] + laser_y - CNC.vars["anchor1_y"] - CNC.vars["anchor2_offset_y"], 4)
+            elif self.cbx_current_position.active:
+                x = 0
+                y = 0
+        self.txt_x_offset.text = str(x)
+        self.txt_y_offset.text = str(y)
 
 class ZProbePopup(ModalView):
     def __init__(self, coord_popup, **kwargs):

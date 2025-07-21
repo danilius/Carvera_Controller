7d96a30d5c39d03bb2551418d8d1189d6faa7fd5
Serge Bakharev <serge.bakharev@gmail.com>
2025-07-21 14:42:50 +1000

    For AppImage builds replace appimage-builder with linuxdeploy

--- scripts/install_linux_prereqs.sh
+++ scripts/install_linux_prereqs.sh
@@ -27,4 +27,17 @@ sudo apt install -y \
     libhidapi-hidraw0
 
 
+print_status "Installing linuxdeploy (AppImage)"
+LINUXDEPLOY_URL="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
+LINUXDEPLOY_BIN="linuxdeploy"
+TMP_LINUXDEPLOY="/tmp/linuxdeploy-x86_64.AppImage"
+curl -L "$LINUXDEPLOY_URL" -o "$TMP_LINUXDEPLOY"
+chmod +x "$TMP_LINUXDEPLOY"
+if [ "$(id -u)" -eq 0 ]; then
+    mv "$TMP_LINUXDEPLOY" "/usr/local/bin/$LINUXDEPLOY_BIN"
+else
+    mkdir -p "$HOME/.local/bin"
+    mv "$TMP_LINUXDEPLOY" "$HOME/.local/bin/$LINUXDEPLOY_BIN"
+fi
+
 print_status "Linux development dependencies installed successfully"

ae32622c1f1a02b98ed170e6d8a12f407de0e98b
Serge Bakharev <serge.bakharev@gmail.com>
2025-08-09 13:15:37 +1000

    Update digitization logic to treat default version 0.0.0 as the latest version by setting major version to 999.

--- carveracontroller/Utils.py
+++ carveracontroller/Utils.py
@@ -439,7 +439,12 @@ def digitize_v(version):
     # Ensure we have at least 3 parts (major.minor.patch)
     while len(cleaned_parts) < 3:
         cleaned_parts.append(0)
-    
+
+    # default controller version (until build script runs) is 0.0.0
+    # This should be treated as the "latest" version
+    if cleaned_parts[0] == 0:
+        cleaned_parts[0] = 999
+
     return cleaned_parts[0] * 1000 * 1000 + cleaned_parts[1] * 1000 + cleaned_parts[2]
 
 #------------------------------------------------------------------------------

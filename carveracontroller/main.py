74401afab55bbd5216bb49df6dd83ff1aef71864
Serge Bakharev <sergebakharev@users.noreply.github.com>
2026-01-27 15:39:57 +1100

    use bigger popup size for all the text

--- carveracontroller/main.py
+++ carveracontroller/main.py
@@ -5492,8 +5492,8 @@ class Makera(RelativeLayout):
         commands = self.controller.playStartLineCommand(file_name, start_line, preview=True, local_file_path=local_file_path)
         commands_preview = '\n'.join(commands)
         
-        self.confirm_popup.size_hint = (0.6, 0.5)
-        self.confirm_popup.pos_hint = {"right": 0.75, "top": 0.7}
+        self.confirm_popup.size_hint = (0.8, 0.8)
+        self.confirm_popup.pos_hint = {"center_x": 0.5, "center_y": 0.5}
         self.confirm_popup.lb_title.text = tr._('Beta Feature: Resume Playback')
         self.confirm_popup.lb_content.text = tr._('The "resume playback at line" functionality is beta. Please be prepared to e-stop your machine if it doesn\'t move correctly.\n\nCommands that will be executed:\n') + commands_preview
         self.confirm_popup.confirm = partial(self.execute_play_with_start_line, file_name, start_line)
